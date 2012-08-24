8f3b5bda7f16c2daac6ac44c62c2a490c32b64cf
Kieran Goodary <kgoodary@gmail.com>
2012-08-24 12:09:33 +0100

    Initial Commit :)

---
+++ application/controllers/site.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Site extends CI_Controller {
+
+
+	public function index() {
+	
+		$this->template->render();
+		
+	}
+}
+
+/* End of file site.php */
+/* Location: ./application/controllers/site.php */
